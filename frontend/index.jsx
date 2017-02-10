8c6e5fb1fae55cd392a3a59d7c3b81767eb09402
Joshua Ling <joshling1919@gmail.com>
2017-02-09 23:07:44 -0800

    set up frontend

---
+++ frontend/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    ReactDOM.render(<h1>Welcome to Taughtspring</h1>, root);
+});
