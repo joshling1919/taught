c3b09042d74f77baf45fa9f54772ecbc9fe543c4
Joshua Ling <joshling1919@gmail.com>
2017-02-10 23:48:50 -0800

    start handroll auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,50 @@
 class User < ApplicationRecord
+
+  attr_reader :password
+
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: {minimum: 8}, allow_nil: :true
+
+  after_initialize :ensure_session_token
+  before_validation :ensure_session_token_uniqueness
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.valid_password?(password) ? user : nil
+  end
+
+  def valid_password?(password)
+    pw_digest = BCrypt::Password.new(self.password_digest)
+    pw_digest.is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = new_session_token
+    ensure_session_token_uniqueness
+    self.save
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= new_session_token
+  end
+
+  def new_session_token
+    SecureRandom.urlsafe_base64(32)
+  end
+
+  def ensure_session_token_uniqueness
+    while User.find_by(session_token: self.session_token)
+      self.session_token = new_session_token
+    end 
+  end
+
 end
